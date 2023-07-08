191244e58072abfe1026bdcc2f1dfd0eb45556a8
Frost <frostedwolf789@gmail.com>
2023-07-08 18:10:36 -0400

    ADD: database connection, update.php, input files

--- main.php
+++ main.php
@@ -1,22 +1,42 @@
+
+<?php
+/*
+$dns = "mysql:host=localhost;dbname=cp476_db;charset=utf8mb4";
+$options = [
+  PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+];
+try{
+  $conn = new PDO($dns, "root", "", $options);
+} catch (Exception $e){
+  error_log($e->getMessage());
+  exit('Something weird happened'); //something a user can understand
+}
+*/
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CP476 Group Project</title>
-    <link rel="stylesheet" href="main_style.css">
-  </head>
-  <body onload="set_username()">
-    <h1 id = "welcome">Welcome User,</h1>
 
-    <div class = "listContainer">
-      <ul class = "btnList">
-        <li> <a href="#"> <button class="dbBtn"><img src = "assests/search.svg"> <br>search</button> </a> </li>
-        <li> <a href="#"> <button class="dbBtn"><img src = "assests/update.svg"> <br>update</button> </a> </li>
-        <li> <a href="#"> <button class="dbBtn"><img src = "assests/delete.svg"> <br>delete</button> </a> </li>
-      </ul>
-    </div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CP476 Group Project</title>
+  <link rel="stylesheet" href="main_style.css">
+</head>
 
-	<script src="index.js"></script>
-  </body
+<body onload="set_username()">
+  <h1 id="welcome">Welcome User,</h1>
+
+  <div class="listContainer">
+    <ul class="btnList">
+      <li> <a href="#"> <button class="dbBtn"><img src="assests/search.svg"> <br>search</button> </a> </li>
+      <li> <a href="update.php"> <button class="dbBtn"><img src="assests/update.svg"> <br>update</button> </a> </li>
+      <li> <a href="#"> <button class="dbBtn"><img src="assests/delete.svg"> <br>delete</button> </a> </li>
+    </ul>
+  </div>
+
+  <script src="index.js"></script>
+</body>
--- sql/sql_create.sql
+++ sql/sql_create.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE CP476_db;
 
 CREATE TABLE `supplier` (
-  `supplier_id` int NOT NULL AUTO_INCREMENT,
+  `supplier_id` int NOT NULL AUTO_INCREMENT, 
   `supplier_name` varchar(50),
   `address` varchar(100),
-  `phone` int,
+  `phone` varchar(50),
   `email` varchar(60),
   PRIMARY KEY (`supplier_id`)
 );
@@ -13,10 +13,20 @@ CREATE TABLE `product` (
   `product_id` int NOT NULL AUTO_INCREMENT,
   `product_name` varchar(50),
   `description` varchar(100),
-  `price` float,
+  `price` varchar(50),
   `quantity` int,
-  `status` char(1),
+  `status` char(2),
   `supplier_id` int,
   PRIMARY KEY (product_id),
   FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id)
 );
+
+SET GLOBAL local_infile=1;
+
+-- quit
+-- mysql --local-infile=1 -u root -p
+-- use CP476_db;
+
+LOAD DATA LOCAL INFILE 'SupplierFile.txt' INTO TABLE supplier FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE 'ProductFile.txt' INTO TABLE product FIELDS TERMINATED BY ',';
