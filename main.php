64e3900a933e54b323119eaba13e47ee7fa049bb
Frost <frostedwolf789@gmail.com>
2023-07-10 13:14:31 -0400

    Add database connection in db.php, remove connection in main.php

--- main.php
+++ main.php
@@ -1,21 +1,3 @@
-
-<?php
-/*
-$dns = "mysql:host=localhost;dbname=cp476_db;charset=utf8mb4";
-$options = [
-  PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
-  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
-];
-try{
-  $conn = new PDO($dns, "root", "", $options);
-} catch (Exception $e){
-  error_log($e->getMessage());
-  exit('Something weird happened'); //something a user can understand
-}
-*/
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,7 +15,7 @@ try{
   <div class="listContainer">
     <ul class="btnList">
       <li> <a href="#"> <button class="dbBtn"><img src="assests/search.svg"> <br>search</button> </a> </li>
-      <li> <a href="update.php"> <button class="dbBtn"><img src="assests/update.svg"> <br>update</button> </a> </li>
+      <li> <a href="update.html"> <button class="dbBtn"><img src="assests/update.svg"> <br>update</button> </a> </li>
       <li> <a href="#"> <button class="dbBtn"><img src="assests/delete.svg"> <br>delete</button> </a> </li>
     </ul>
   </div>
