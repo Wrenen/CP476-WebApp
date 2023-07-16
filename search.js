985581ff1ffdc0a38cee385ce37c8fb1c27b7445
abezt <andrew.c.best1@gmail.com>
2023-07-15 22:54:47 -0400

    added search sql query function // updated css

--- search.js
+++ search.js
@@ -6,6 +6,8 @@ var searchFields = document.getElementById("searchField");
 var supplier = ['supplier_id', 'supplier_name', 'address', 'phone', 'email'];
 var product = ['pid', 'product_id', 'product_name', 'description', 'price', 'quantity','status', 'supplier_id'];
 
+var field_order  = [];
+
 var counter = 0;
 
 // resets page back to default
@@ -13,6 +15,7 @@ function changeTable() {
     addForm.style.display = "none";
     document.getElementById("search-fields").innerHTML = "";
     clearFields();
+    counter = 0;
 }
 
 function clearFields() {
@@ -23,38 +26,55 @@ function clearFields() {
 
 /*  <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"> */
 function addSearchField() {
-    addForm.style.display = "none";
-    // clearFields();
-    var newSearch = addFields.value;
+    if (addFields.value != "-") {
+        console.log(addFields.value);
+        addForm.style.display = "none";
+        document.querySelector('select[name="add-fields"] option[value="' + addFields.value + '"]').disabled = true;
 
-    const div = document.createElement('div');
-    div.className = "form-group";
-    div.id = counter;
+        field_order.unshift(addFields.value);
+        var newSearch = addFields.value;
 
-    div.innerHTML = `
-    <label for="` + (newSearch + counter) + `" class="col-sm-2 col-form-label input-labels">` + (newSearch + ' =') + `</label>
-    <div class="col-sm-10">
-        <input type="text" class="form-control search-input" id="` + (newSearch + counter) + `" placeholder="` + newSearch + `">
-    </div>
-    `;
-    console.log(div);
-    document.getElementById("search-fields").appendChild(div);
+        const div = document.createElement('div');
+        div.className = "form-group";
 
-    counter += 1;
+        div.innerHTML = `
+        <label for="` + counter + `" class="col-sm-2 col-form-label input-labels">` + (newSearch + ' =') + `</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control search-input" id="`+ counter + `"name="` + ("input-"+counter) + `" placeholder="` + newSearch + `">
+            <input type="hidden" name="` + ("field-"+counter) + `" value="` + (newSearch) + `">
+        </div> 
+        `;
+        console.log(div);
+        document.getElementById("search-fields").appendChild(div);
+
+        counter += 1;
+    }
 }
 
 function removeSearchField() {
     let element = document.getElementById("search-fields");
-    element.removeChild(element.lastChild);
-    counter -= 1;
+
+    if (element.childNodes.length > 0) {
+        document.querySelector('select[name="add-fields"] option[value="' + field_order.shift() + '"]').disabled = false;
+        console.log(element.lastChild);
+        element.removeChild(element.lastChild);
+        counter -= 1;
+    }
 }
 
 function addFieldVals() {
     var tableSelected = (tableField.value == 'supplier') ? supplier : product;
     
     if (addFields.options.length == 0) {
+        var newOption = document.createElement('option');
+        newOption.setAttribute('disabled', true);
+        newOption.setAttribute('selected', true);
+        newOption.setAttribute('value', "-");
+        newOption.innerHTML = "-- select an option --";
+        addFields.add(newOption);
+
         for (let i = 0; i != tableSelected.length; i++) {
-            var newOption = document.createElement('option');
+            newOption = document.createElement('option');
             newOption.setAttribute('value', tableSelected[i]);
             newOption.innerHTML = tableSelected[i];
             addFields.add(newOption);
@@ -64,5 +84,6 @@ function addFieldVals() {
 
 function addBtn() {
     addForm.style.display = "block";
+    addFields.value = '-';
     addFieldVals();
 }
---
+++ search_results.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search Results</title>
+    <link rel="stylesheet" href="search_style.css">
+</head>
+<body>
+
+<?php
+require_once('db.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the submitted table value
+    $table = $_POST['table'];
+
+    // Retrieve the submitted column values
+    $fields = array();
+    $values = array();
+    $init_count = 0;
+
+    $stmt_prep = "SELECT * FROM $table";
+    $stmt_vals = array();
+    
+    while(true) {
+        $field_name = 'field-' . $init_count;
+        $field_val = 'input-' . $init_count;
+        if (isset($_POST[$field_name]) && isset($_POST[$field_val])) {
+            array_push($fields, $_POST[$field_name]);
+            array_push($values, trim(htmlspecialchars($_POST[$field_val])));          
+        }else {
+            break;
+        }
+        $init_count += 1;
+    }
+
+    // Display search paramters names // generate sql prep statement
+    echo "<h2>Query Information</h2>";
+    echo "<p><strong>Selected Table: </strong>$table</p>";
+    echo "<p><strong>Search Parameters:</strong></p>";
+    
+    if ($init_count > 0){
+        $stmt_prep .= " WHERE ";
+        for ($i = 0; $i < count($fields); $i++) {
+            $field = $fields[$i];
+            $value = $values[$i];
+
+            $val_list = explode(",", $value);
+            $val_len = count($val_list);
+            $sub_stmt = "(";
+
+            if ($val_len > 1) {
+                for ($j = 0; $j < $val_len; $j++) {
+                    array_push($stmt_vals, $val_list[$j]);
+                    $sub_stmt .= ($j != $val_len - 1) ? "$field = ? OR " : "$field = ?)";
+                }
+                $stmt_prep .= ($i != count($fields) - 1) ?  "$sub_stmt AND ": "$sub_stmt";
+            }else {
+                array_push($stmt_vals, $value);
+                $stmt_prep .= ($i != count($fields) - 1) ? "$field = ? AND ": "$field = ?";
+            }
+            echo "<p> $field = $value</p>";
+        }
+    }
+    $stmt_len=count($stmt_vals);
+    echo "<p><strong>Statement:</strong> $stmt_prep, <strong>indexes:</strong> $stmt_len</p>";
+
+    $stmt = $conn->prepare($stmt_prep);
+
+    // add " " to strings to account for extra space in data naming
+    for ($i = 0; $i < $stmt_len; $i++) {
+        if (is_string($stmt_vals[$i])) {
+            $binding_val = " ". $stmt_vals[$i];
+        } else {
+            $binding_val = $stmt_vals[$i];
+        }
+        $stmt->bindValue($i + 1, $binding_val);
+    }
+
+    $stmt->execute();
+    $resultsTable = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($resultsTable) > 0) {
+        echo "<table border=1>";
+        echo "<tr>";
+        foreach ($resultsTable[0] as $col => $val) {
+            echo "<th>$col</th>";
+        }
+        echo "</tr>";
+        foreach ($resultsTable as $row) {
+            echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }else {
+        echo "<p> -- No Matches -- </p>";
+    }
+}
+?>
+</body>
+</html>
